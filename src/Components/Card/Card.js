0c4a75232ab6269843995d3b09400c098347c017
unknown <juanstovarm@gmail.com>
2025-03-26 18:35:22 -0500

    simulacro1.1

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,47 +1,27 @@
-class CardCustom extends HTMLElement {
-    constructor(){
-        super();
-        this.attachShadow({mode: 'open'});
-    }
-    static get observedAttributes() {
-        return ['id','category', 'title', 'description', 'image','author','date'];
-    }
-    
-    connectedCallback() {
-        this.render();
-    }
+class Card extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+   
+  }
+  static get observedAttributes() {
+    return ['name', 'status', 'type', 'image'];
+  }
 
-    render(){
-        const imageUrl = this.getAttribute('image');
-        const hasImage = imageUrl && imageUrl.trim() !== '';
-        this.shadowRoot.innerHTML = `
-            <style>
+  connectedCallback() {
+    this.render();
+  }
 
-                .card {
-                    display: flex;
-                    flex-direction: row;
-                    border-radius: 10px;
-                    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-                    overflow: hidden;
-                    margin: 10px;
-                    background-color: #fff;
-                }
-
-                
-            </style>
-        
-            <div class="card">
-            <img src="${this.getAttribute('image')}" alt="${this.getAttribute('name')}>
-                <p>Name: ${this.getAttribute('name')}</p>
-                <p>Status: ${this.getAttribute('status')}</p>
-                <p>Type: ${this.getAttribute('type')}</p>
-                
-            </div>
-        `;
+  render() {
+    this.shadowRoot.innerHTML = `
+    <div>
+        <img src="${this.getAttribute('image')}" alt="${this.getAttribute('name')}">
+        <p>Name: ${this.getAttribute('name')}</p>
+        <p>Status: ${this.getAttribute('status')}</p>
+        <p>Type: ${this.getAttribute('type')}</p>
+    </div>
+    `
     }
-        
-        
-    
 }
 
-export default CardCustom;
+export default Card;
